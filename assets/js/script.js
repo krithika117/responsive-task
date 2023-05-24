d1963fb3cc357c7e09804fe71c991aada5a8b0be
krithika117 <krithikanithyanandam1@gmail.com>
2023-05-24 22:29:57 +0530

    Add. teams layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,115 +1,163 @@
-const BASE_URL = "https://krithika-marvel-api.vercel.app";
-// const BASE_URL = "http://127.0.0.1:3000";
-const dropdown = document.getElementById('planet-dropdown');
-const planetNameElement = document.getElementById('planet-name');
-const planetInfoElement = document.getElementById('planet-info');
-const planetContainer = document.getElementById('planet-container');
+if (window.location.pathname == "/universe.html") {
+    const BASE_URL = "https://krithika-marvel-api.vercel.app";
+    // const BASE_URL = "http://127.0.0.1:3000";
+    const dropdown = document.getElementById('planet-dropdown');
+    const planetNameElement = document.getElementById('planet-name');
+    const planetInfoElement = document.getElementById('planet-info');
+    const planetContainer = document.getElementById('planet-container');
 
-fetch(BASE_URL + '/planets')
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(planet => {
-            const option = document.createElement('option');
-            option.value = planet.name;
-            option.textContent = planet.name;
-            if (planet.name === 'Vormir') {
-                option.selected = true; // Set "Vormir" as the default selected value
-                displayPlanetInfo('Vormir');
-            }
-            dropdown.appendChild(option);
+    fetch(BASE_URL + '/planets')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(planet => {
+                const option = document.createElement('option');
+                option.value = planet.name;
+                option.textContent = planet.name;
+                if (planet.name === 'Vormir') {
+                    option.selected = true;
+                    displayPlanetInfo('Vormir');
+                }
+                dropdown.appendChild(option);
+            });
+        })
+        .catch(error => {
+            console.log('Error:', error);
         });
-    })
-    .catch(error => {
-        console.log('Error:', error);
+
+    document.getElementById('planet-dropdown').addEventListener('change', (e) => {
+        const selectedPlanet = e.target.value;
+        displayPlanetInfo(selectedPlanet);
     });
 
-document.getElementById('planet-dropdown').addEventListener('change', (e) => {
-    const selectedPlanet = e.target.value;
-    displayPlanetInfo(selectedPlanet);
-});
+    async function displayPlanetInfo(selectedPlanet) {
+        try {
+            const response = await fetch(`${BASE_URL}/planets/${selectedPlanet}`);
+            const data = await response.json();
 
-async function displayPlanetInfo(selectedPlanet) {
-    try {
-        const response = await fetch(`${BASE_URL}/planets/${selectedPlanet}`);
-        const data = await response.json();
+            planetContainer.className = 'planet ' + selectedPlanet.toLowerCase();
+            planetNameElement.textContent = data.stone ? `${selectedPlanet} ${data.stone}` : selectedPlanet;
 
-        planetContainer.className = 'planet ' + selectedPlanet.toLowerCase();
-        planetNameElement.textContent = data.stone ? `${selectedPlanet} ${data.stone}` : selectedPlanet;
-
-        let info = '';
-        if (data.stone) {
-            info = `${data.stone}`;
-            if (data.distance) {
-                info += ` and Thanos is ${data.distance} ⚠️`;
+            let info = '';
+            if (data.stone) {
+                info = `${data.stone}`;
+                if (data.distance) {
+                    info += ` and Thanos is ${data.distance} ⚠️`;
+                } else {
+                    info += ` but Thanos is not a threat`;
+                }
             } else {
-                info += ` but Thanos is not a threat`;
+                info = 'No additional information available.';
             }
-        } else {
-            info = 'No additional information available.';
+
+            const cardBody = document.createElement('div');
+            cardBody.className = 'card-body p-3';
+            const cardTitle = document.createElement('h5');
+            cardTitle.className = 'card-title';
+            cardTitle.textContent = selectedPlanet;
+            const cardText = document.createElement('p');
+            cardText.className = 'card-text';
+            cardText.textContent = info;
+
+            const card = document.createElement('div');
+            card.className = 'card blue';
+            card.appendChild(cardBody);
+
+            cardBody.appendChild(cardTitle);
+            cardBody.appendChild(cardText);
+
+            const cardContainer = document.querySelector('#planet-info');
+            cardContainer.innerHTML = '';
+            if (data.distance) {
+                cardText.classList.add('blinking');
+                const notifyButton = document.createElement('button');
+                notifyButton.textContent = 'Notify Troop';
+                notifyButton.className = "btn btn-secondary bg-danger";
+                notifyButton.addEventListener('click', () => {
+                    sendEmail(selectedPlanet, data);
+                });
+
+                cardBody.append(notifyButton);
+            }
+            cardContainer.appendChild(card);
+
+        } catch (error) {
+            console.log('Error:', error);
         }
+    }
 
-        const cardBody = document.createElement('div');
-        cardBody.className = 'card-body p-3';
-        const cardTitle = document.createElement('h5');
-        cardTitle.className = 'card-title';
-        cardTitle.textContent = selectedPlanet;
-        const cardText = document.createElement('p');
-        cardText.className = 'card-text';
-        cardText.textContent = info;
+    // Incomplete send email function
+    async function sendEmail(selectedPlanet, data) {
+        (function () {
+            emailjs.init('tqAWXJdq654vqEETQ');
+        })();
 
-        const card = document.createElement('div');
-        card.className = 'card blue';
-        card.appendChild(cardBody);
+        const templateParams = {
+            to_name: "krithika.23it@licet.ac.in",
+            planet: selectedPlanet,
+            stone: data.stone,
+        };
 
-        cardBody.appendChild(cardTitle);
-        cardBody.appendChild(cardText);
+        const notifyButton = document.querySelector('.btn.bg-danger');
+        notifyButton.textContent = 'Deployed Forces';
+        notifyButton.className = 'btn bg-success'
+        notifyButton.disabled = true;
 
-        const cardContainer = document.querySelector('#planet-info');
-        cardContainer.innerHTML = '';
-        if (data.distance) {
-            cardText.classList.add('blinking');
-            const notifyButton = document.createElement('button');
-            notifyButton.textContent = 'Notify Troop';
-            notifyButton.className = "btn btn-secondary bg-danger";
-            notifyButton.addEventListener('click', () => {
-                sendEmail(selectedPlanet, data);
+        emailjs.send("service_rmkmuho", "template_bfhxk6t", templateParams, "tqAWXJdq654vqEETQ")
+            .then(function (response) {
+                console.log('Email sent:', response.status, response.text);
+            }, function (error) {
+                console.log('Error:', error);
+                // In case of error, reset the button text and enable it again
+                notifyButton.textContent = 'Notify Troop';
+                notifyButton.disabled = false;
             });
-
-            cardBody.append(notifyButton);
-        }
-        cardContainer.appendChild(card);
-
-    } catch (error) {
-        console.log('Error:', error);
     }
 }
+if (window.location.pathname == "/team.html") {
+    const apiURL =
+        'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=2c764864c5c72c7f6bf98584d2969e45&hash=91d69e2b93b176b0c9fb00841c1f805b';
+    const characterContainer = document.getElementById('character-container');
 
-// Incomplete send email function
-async function sendEmail(selectedPlanet, data) {
-    (function () {
-        emailjs.init('tqAWXJdq654vqEETQ');
-    })();
+    fetch(apiURL)
+        .then(response => response.json())
+        .then(data => {
+            const characters = data.data.results;
+            characters.forEach(character => {
+                const name = character.name;
+                const imageUrl = `${character.thumbnail.path}.${character.thumbnail.extension}`;
 
-    const templateParams = {
-        to_name: "krithika.23it@licet.ac.in",
-        planet: selectedPlanet,
-        stone: data.stone,
-    };
+                const card = document.createElement('div');
+                card.className = 'card blue d-flex';
+                
+                const imageContainer = document.createElement('div');
+                imageContainer.className = 'image-container align-self-start';
+                
+                const image = document.createElement('img');
+                image.src = imageUrl;
+                image.alt = name;
+                image.className = 'image';
+                
+                const cardBody = document.createElement('div');
+                cardBody.className = 'card-body ms-auto';
+                
+                const cardTitle = document.createElement('h5');
+                cardTitle.className = 'card-title';
+                cardTitle.textContent = name;
+                
+                // Add elements to card
+                imageContainer.appendChild(image);
+                cardBody.appendChild(cardTitle);
+                
+                card.appendChild(imageContainer);
+                card.appendChild(cardBody);
+                
 
-    const notifyButton = document.querySelector('.btn.bg-danger');
-    notifyButton.textContent = 'Deployed Forces';
-    notifyButton.className = 'btn bg-success'
-    notifyButton.disabled = true;
 
-    emailjs.send("service_rmkmuho", "template_bfhxk6t", templateParams, "tqAWXJdq654vqEETQ")
-        .then(function (response) {
-            console.log('Email sent:', response.status, response.text);
-        }, function (error) {
+                // Add card to container
+                characterContainer.appendChild(card);
+            });
+        })
+        .catch(error => {
             console.log('Error:', error);
-            // In case of error, reset the button text and enable it again
-            notifyButton.textContent = 'Notify Troop';
-            notifyButton.disabled = false;
         });
 }
-
-const currentTimestamp = Math.floor(Date.now() / 1000); 
